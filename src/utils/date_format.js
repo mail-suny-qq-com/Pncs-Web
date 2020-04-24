0095568d75bb5e31b8e74c4c68d2854c780c8ea3
lf <lf>
2020-04-24 17:38:17 +0800

    指标管理

--- src/utils/date_format.js
+++ src/utils/date_format.js
@@ -1,13 +1,21 @@
-let moment = require('moment')
+const moment = require('moment')
 export function dateColumnFormat(row, column, cellValue, index) {
-  //console.log('===========4444=================', cellValue)
-  return moment(cellValue).format('YYYY-MM-DD')
-  //return moment(row.crtDate).format('YYYY-MM-DD HH:mm:ss');
+  // console.log('===========4444=================', cellValue)
+  if (cellValue != null) {
+    return moment(cellValue).format('YYYY-MM-DD')
+  } else {
+    return ''
+  }
+  // return moment(row.crtDate).format('YYYY-MM-DD HH:mm:ss');
 }
 export function timeColumnFormat(row, column, cellValue, index) {
-  //console.log('===========55=================', cellValue)
-  return moment(cellValue).format('YYYY-MM-DD HH:mm:ss')
-  //return moment(row.crtDate).format('YYYY-MM-DD HH:mm:ss');
+  // console.log('===========55=================', cellValue)
+  if (cellValue != null) {
+    return moment(cellValue).format('YYYY-MM-DD HH:mm:ss')
+  } else {
+    return ''
+  }
+  // return moment(row.crtDate).format('YYYY-MM-DD HH:mm:ss');
 }
 
-export default {dateColumnFormat,timeColumnFormat}
+export default { dateColumnFormat, timeColumnFormat }
