ce138a5c15913b7b4c3bfc4245c996158c98705b
suny_mail <33649615+Suny999@users.noreply.github.com>
2020-04-27 11:13:15 +0800

    init

--- src/api/smartbi.js
+++ src/api/smartbi.js
@@ -2,8 +2,16 @@ import request from '@/utils/request'
 
 export function login(username) {
   return request({
-    url: '/smartbi/login/'+username,
+    url: '/smartbi/login/' + username,
     method: 'post'
   })
 }
-export default { login }
+
+export function syncUser(url, method) {
+  return request({
+    url: url,
+    method: method
+  })
+}
+
+export default { login, syncUser }
